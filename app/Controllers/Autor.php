7ea7e7a476413a309105b0b81ff56cdde070f84f
Heden <hedensilva@gmail.com>
2024-05-09 11:23:06 -0300

    Alterar AutorController

--- app/Controllers/Autor.php
+++ app/Controllers/Autor.php
@@ -4,7 +4,7 @@ namespace App\Controllers;
 
 use App\Controllers\BaseController;
 use CodeIgniter\HTTP\ResponseInterface;
-use App\models\AutorModel;
+use App\Models\AutorModel;
 
 class Autor extends BaseController
 {
